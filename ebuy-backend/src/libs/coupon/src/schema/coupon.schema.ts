16e9a2987354ce50e35f36975152fb703cd84861
manh117bg@gmail.com <manh117bg@gmail.com>
2021-01-12 00:40:25 +0700

    update coupon API

---
+++ ebuy-backend/src/libs/coupon/src/schema/coupon.schema.ts
@@ -0,0 +1,31 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+import { DocumentNode } from "src/utils/document-node.schema";
+
+@Schema()
+export class Coupon extends DocumentNode {
+
+    @Prop({type: String, required: true, default: 'COUPON_CODE'})
+    code: string
+
+    @Prop({type: Number, required: true, default: 10})
+    percentage: number
+
+    @Prop({type: Number, required: true, default: 15})
+    maxDiscount: number
+
+    @Prop({type: Date, required: true, default: Date.now()})
+    startDate: Date
+
+    @Prop({type: Date, required: true, default: Date.now()})
+    endDate: Date
+
+    @Prop({type: Number, required: true, default: 10})
+    maxNumber: number
+
+    @Prop({type: Number, required: true, default: 100})
+    orderCondition: number
+}
+
+export const CouponSchema = SchemaFactory.createForClass(Coupon)
+export type CouponDocument = Document & Coupon
